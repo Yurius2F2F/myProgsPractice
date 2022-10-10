4efa2bdf0a295d2f9153e0003df31199a3266328
Yurius <yurik110990@yandex.ru>
2022-10-11 00:08:01 +0400

    Добавил Задачу 6 (task6)

---
+++ task6/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 6: Напишите программу, которая 
+// 1. на вход принимает число 
+// 2. и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else Console.WriteLine("Нет");
