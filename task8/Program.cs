67ca9228de9e47d73dde8f538da7b60f86f324a4
Yurius <yurik110990@yandex.ru>
2022-10-11 12:33:27 +0400

    Добавил Задачу 8 (task8)

---
+++ task8/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 8: Напишите программу, которая 
+// 1. на вход принимает число (N), 
+// 2. а на выходе показывает все чётные числа 
+// 3. от 1 до N.
+
+Console.WriteLine("Введите целое число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Все целые числа от 1 до {number}: ");
+int counter = 1;
+while (counter <= number)
+{
+    if (counter % 2 == 0)
+    {
+        Console.Write($"{counter} ");
+        counter += 2;
+    }
+    else counter++;
+}
