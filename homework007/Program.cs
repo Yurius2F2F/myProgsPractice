095e8d3893e615f8fc5d16a7bbd66b1cf033242e
Yurius <yurik110990@yandex.ru>
2022-10-11 21:07:20 +0400

    Добавил новое решение в homework 007

--- homework007/Program.cs
+++ homework007/Program.cs
@@ -12,3 +12,7 @@ Console.WriteLine($"Случайное число из диапазона 100 - 
 int firstDigit = number / 100; // 456 / 100 = 4.56 -> 4
 int secondDigit = number % 10; // 456 % 10 = 6 - остаток
 Console.WriteLine($"Двухзначное число без среднего = {firstDigit}{secondDigit}");
+
+// еще можно так:
+// int twoDigits = firstDigit * 10 + secondDigit;
+// Console.WriteLine($"Двухзначное число без среднего = {twoDigits}");
