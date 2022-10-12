e8efc71c496c74b43b1e5f5ede540450806a8001
Yurius <yurik110990@yandex.ru>
2022-10-12 20:53:01 +0400

    Добавил homework 009 и homework 010

---
+++ homework009/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 14. Напишите программу, которая 
+// 1. принимает на вход число 
+// 2. и проверяет, кратно ли оно одновременно 7 и 23.
+
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+bool CheckingNumber(int number)
+{
+   return number % 7 == 0 && number % 23 == 0;
+   
+}
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+bool result = CheckingNumber(num);
+if(result)
+{
+    Console.Write("Да");
+}
+else{Console.Write("Нет");}
---
+++ homework010/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 16. Напишите программу, которая 
+// 1. принимает на вход два числа 
+// 2. и проверяет, является ли одно число квадратом другого.
+
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+bool CheckingNumbers(int number1, int number2)
+{
+    int min = number1;
+    int max = number2;
+    if (number1 > number2)
+    {
+        min = number2;
+        max = number1;
+    }
+    return min * min == max;
+  
+}
+bool result = CheckingNumbers(num1, num2);
+if (result) Console.Write("Да");
+else Console.Write("Нет");
