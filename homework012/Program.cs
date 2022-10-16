f5a1494039286303132a503b30674b70c3580181
Yurius <yurik110990@yandex.ru>
2022-10-17 00:25:05 +0400

    Исправлена homework 012 - новое решение

--- homework012/Program.cs
+++ homework012/Program.cs
@@ -3,16 +3,16 @@
 // 2. показывает диапазон возможных координат точек в этой четверти (x и y).
 
 System.Console.WriteLine("Введите номер четверти: ");
-int num = Convert.ToInt32(Console.ReadLine());
+string num = Console.ReadLine();
 
 string result = Range(num);
 Console.WriteLine(result);
 
-string Range(int number)
+string Range(string number)
 {
-    if (number == 1) return "x > 0 и y > 0";
-    if (number == 2) return "x < 0 и y > 0";
-    if (number == 3) return "x < 0 и y < 0";
-    if (number == 4) return "x > 0 и y < 0";
+    if (number == "1") return "x > 0 и y > 0";
+    if (number == "2") return "x < 0 и y > 0";
+    if (number == "3") return "x < 0 и y < 0";
+    if (number == "4") return "x > 0 и y < 0";
     return "Ошибка ввода - нет такой четверти!";
 }
