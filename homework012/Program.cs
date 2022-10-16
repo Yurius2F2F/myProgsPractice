56506cd6d5a999b71a2b03eec9f083edea787c6b
Yurius <yurik110990@yandex.ru>
2022-10-17 00:18:58 +0400

    Добавлена homework 012

---
+++ homework012/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 18: Напишите программу, которая 
+// 1. по заданному номеру четверти, 
+// 2. показывает диапазон возможных координат точек в этой четверти (x и y).
+
+System.Console.WriteLine("Введите номер четверти: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string result = Quarter(num);
+Console.WriteLine(result);
+
+string Quarter(int number)
+{
+    if (number == 1) return "x > 0 и y > 0";
+    if (number == 2) return "x < 0 и y > 0";
+    if (number == 3) return "x < 0 и y < 0";
+    if (number == 4) return "x > 0 и y < 0";
+    return "Ошибка ввода - нет такой четверти!";
+}
