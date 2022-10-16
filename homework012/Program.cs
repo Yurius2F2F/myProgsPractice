5f8cd6949ccd02586f7eabc534cd68165bd05ec4
Yurius <yurik110990@yandex.ru>
2022-10-17 00:20:19 +0400

    Исправлена homework 012

--- homework012/Program.cs
+++ homework012/Program.cs
@@ -5,10 +5,10 @@
 System.Console.WriteLine("Введите номер четверти: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
-string result = Quarter(num);
+string result = Range(num);
 Console.WriteLine(result);
 
-string Quarter(int number)
+string Range(int number)
 {
     if (number == 1) return "x > 0 и y > 0";
     if (number == 2) return "x < 0 и y > 0";
