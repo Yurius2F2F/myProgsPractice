09542395f8048c72d0a7f997e8aba9c171356184
Yurius <yurik110990@yandex.ru>
2022-10-17 10:55:47 +0400

    Добавлен homework014

---
+++ homework014/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 22: Напишите программу, которая
+// 1. принимает на вход число (N)
+// 2. и выдаёт таблицу квадратов чисел от 1 до N.
+
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+Console.WriteLine("Введите количество чисел, которые необходимо возвести в квадрат: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while (count <= num)
+{
+    Console.WriteLine(Square(count));
+    count++;
+}
+string Square(int number)
+{
+    return ($"|{number, 4} | -> |{number * number, 5}|");
+}
