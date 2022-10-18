bad9eb4f80db9fbb5ffebb22805fa581a716b416
Yurius <yurik110990@yandex.ru>
2022-10-18 21:40:27 +0400

    Добавлена Задача 23

---
+++ task23/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 23
+// Напишите программу, которая 
+// 1. принимает на вход число (N) 
+// 2. и выдаёт таблицу кубов чисел от 1 до N.
+
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+Console.WriteLine("Введите количество чисел, которые необходимо возвести в куб: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while (count <= num)
+{
+    Console.WriteLine(Cube(count));
+    count++;
+}
+string Cube(int number)
+{
+    return ($"|{number, 4} | -> |{number * number * number, 5}|");
+}
