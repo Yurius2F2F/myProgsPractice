5d790dccd4a2fe9c88d440f006dda2f657580255
Yurius <yurik110990@yandex.ru>
2022-10-26 23:14:03 +0400

    Добвлена homework017

--- homework017/Program.cs
+++ homework017/Program.cs
@@ -13,11 +13,41 @@ int Multiplication(int num)
     int prod = 1;
     for (int i = 1; i <= num; i++)
     {
+        // проверяем на переполнение перед выдачей результата
+        checked
+        {
         prod = prod * i;
         // prod *= i;
+        }
     }
     return prod;
 }
 
 int multiplication = Multiplication(number);
 Console.WriteLine($"Результат произведения от 1 до {number} = {multiplication}");
+
+// решение от преподавателя
+
+// long Factorial(long num)
+// {
+// long fact = 1;
+// int i;
+// for (i = 1; i <= num; i++)
+// {
+// try
+// {
+// checked
+// {
+// fact = fact * i;
+// }
+// }
+// catch (Exception)
+// {
+// System.Console.WriteLine("Переполнение типа!");
+// break;
+// }
+// }
+
+// System.Console.WriteLine($"Факториал числа {i - 1} = {fact}");
+// return fact;
+// }
